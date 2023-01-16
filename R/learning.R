5a3afb10aa2db4afc8d9f6b6c10bb06167c32117
Elise Nordin <elise.nordin@gmail.com>
2023-01-16 15:13:55 +0100

    test

---
+++ R/learning.R
@@ -0,0 +1,46 @@
+10
+1+1
+
+# Rbasics -----------------------------------------------------------------
+
+
+
+# test 2 ------------------------------------------------------------------
+
+
+colnames(airquality)
+
+# Object names
+DayOne
+T <- FALSE
+c <- 9
+
+# Spacing
+x[,1]
+x[ ,1]
+mean (x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+height<-feet*12+inches
+df $ z
+x <- 1 : 10
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+
+
+
+library(r3)
+r3::check_git_config()
+
+
+
+
+
+
+
+
+
+
+
+

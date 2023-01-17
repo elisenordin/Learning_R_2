d047e4c86dbaeef0f90484a2a5179cddf40c1a24
Elise Nordin <elise.nordin@gmail.com>
2023-01-17 10:59:04 +0100

    looking at data

--- test_tidyverse.R
+++ test_tidyverse.R
@@ -1,5 +1,12 @@
 library(tidyverse)
 library(NHANES)
 
+# Looking at data ---------------------------------------------------------
+
 # Briefly glimpse contents of dataset
 glimpse(NHANES)
+
+select(NHANES, Age)
+select(NHANES, Age, Weight, BMI)
+select(NHANES, -HeadCirc)
+select(NHANES, starts_with("BP"))
